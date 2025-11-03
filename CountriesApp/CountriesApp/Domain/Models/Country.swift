e44b4a25ee2f9fd9b9384ff5c385938a499c31bd
Haitham Gado <taegado@gmail.com>
2025-11-03 15:54:38 +0200

    eat(domain): add Currency model and wire into Country
    - Add Domain/Models/Currency.swift { code, name? }
    - Update Country to use [Currency]? and keep Identifiable

--- CountriesApp/CountriesApp/Domain/Models/Country.swift
+++ CountriesApp/CountriesApp/Domain/Models/Country.swift
@@ -5,16 +5,17 @@
 //  Created by Haitham Gado on 01/11/2025.
 //
 
+public struct Country: Identifiable, Codable, Equatable, Hashable {
+    public let name: String
+    public let alpha2Code: String
+    public let capital: String?
+    public let currencies: [Currency]?   // ← uses the type above
+    public var id: String { alpha2Code }
 
-struct Country: Identifiable, Hashable {
-    let id: String
-    let name: String
-    let alpha2Code: String
-    let capital: String?
-    let currencies: [Currency]?
-    struct Currency: Hashable { let code: String?; let name: String? }
-    init(name: String, alpha2Code: String, capital: String?, currencies: [Currency]?) {
-        self.id = alpha2Code; self.name = name; self.alpha2Code = alpha2Code
-        self.capital = capital; self.currencies = currencies
+    public init(name: String, alpha2Code: String, capital: String?, currencies: [Currency]?) {
+        self.name = name
+        self.alpha2Code = alpha2Code
+        self.capital = capital
+        self.currencies = currencies
     }
 }
