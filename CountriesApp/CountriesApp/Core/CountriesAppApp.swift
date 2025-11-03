542c7d66de7f5b372e937ee9872d1cf321f012d2
Haitham Gado <taegado@gmail.com>
2025-11-03 15:59:51 +0200

    feat(app): BootstrapView + attach SwiftData container
    - CountriesAppApp: use .modelContainer(for: [CountrySD, PinnedSD])
    - Add BootstrapView to build VM with SwiftDataCountriesStore(context:)
    - Remove manual ModelContext construction/try! usage

--- CountriesApp/CountriesApp/Core/CountriesAppApp.swift
+++ CountriesApp/CountriesApp/Core/CountriesAppApp.swift
@@ -5,21 +5,38 @@
 //  Created by Haitham Gado on 31/10/2025.
 //
 
+
 import SwiftUI
 import SwiftData
-
 
 @main
 struct CountriesAppApp: App {
     var body: some Scene {
         WindowGroup {
-            let api     = APIClient(config: NetworkConfig(baseURL: NetworkConfig.defaultBaseURL))
-            let repo    = CountriesRepositoryImpl(api: api)
-            let useCase = GetAllCountries(repo: repo)
-            CountriesListView(vm: .init(getAll: useCase))
+            BootstrapView()   // builds VM using the environment ModelContext
         }
+        .modelContainer(for: [CountrySD.self, PinnedSD.self]) // attach the real container once
     }
 }
 
+private struct BootstrapView: View {
+    @Environment(\.modelContext) private var context
 
+    var body: some View {
+        // Build dependencies
+        let baseURL = NetworkConfig.defaultBaseURL
+        let api     = APIClient(config: NetworkConfig(baseURL: baseURL))
+        let repo    = CountriesRepositoryImpl(api: api)
+        let useCase = GetAllCountries(repo: repo)
 
+        let store   = SwiftDataCountriesStore(context: context)
+
+        // Hand the VM to the view
+        CountriesListView(
+            vm: CountriesListViewModel(
+                getAll: useCase,
+                persistence: store
+            )
+        )
+    }
+}
